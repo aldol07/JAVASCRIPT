e46f8cb45060c0d10a9e0cc5d9142270c6e1ca94
dubeykartikay1 <149271169+dubeykartikay1@users.noreply.github.com>
2024-09-08 19:08:14 +0000

    conversion of datatypes

---
+++ 01_basics/03_Conversionoperation.js
@@ -0,0 +1,30 @@
+let score = null
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedin = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+//"" => false
+//"kartikay" => true
+
+
+let sumNum = 33
+
+let strimngNum = String(sumNum)
+console.log(strimngNum);
+console.log(typeof strimngNum);
+
+
