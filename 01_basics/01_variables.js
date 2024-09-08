0c64cf0f58c16f954bbb14544b399701dc81b450
dubeykartikay1 <149271169+dubeykartikay1@users.noreply.github.com>
2024-09-08 18:39:20 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 14445
+let accountEmail = "kartikay@gmaiil.com"
+var accountPassword = "12245"
+accountCity = "allahabad"
+// accountID = 2
+accountEmail = "hs@g.com"
+accountPassword = "546541"
+accountCity = "balia"
+let accountState;
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+
+
+*/
+
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountState])
