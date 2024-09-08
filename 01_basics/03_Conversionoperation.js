4ac015b0e5e451e4a904b5b2586b5fd13b9eb77c
dubeykartikay1 <149271169+dubeykartikay1@users.noreply.github.com>
2024-09-08 19:23:40 +0000

    learned dtype conversion

--- 01_basics/03_Conversionoperation.js
+++ 01_basics/03_Conversionoperation.js
@@ -24,7 +24,55 @@ let booleanIsLoggedIn = Boolean(isLoggedin)
 let sumNum = 33
 
 let strimngNum = String(sumNum)
-console.log(strimngNum);
-console.log(typeof strimngNum);
+// console.log(strimngNum);
+// console.log(typeof strimngNum);
+
+
+// ************* Operations****************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//+
+//-
+//*
+//** 
+//*** 
+// /
+// %
+
+let str1 = "Hello"
+let str2 = " Kartikay"
+
+let str3 = str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2)
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+// not a standard method to write code 
+
+let gamecounter = 100
+gamecounter++;
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
 
 
