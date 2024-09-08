4e34a37ee46b6e58a19e6470291ea101226b0dbc
dubeykartikay1 <149271169+dubeykartikay1@users.noreply.github.com>
2024-09-08 18:53:21 +0000

    learned dataset

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treats all JS Code as newer version
+// alert(3+3) // we are using nodejs not browser
+
+
+
+
+
+let name = "Kartikay"
+let age = 21
+let isLoggedin = false
+let state;
+
+// number =>2 to power 53
+//bigint
+//string => "", ''
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
