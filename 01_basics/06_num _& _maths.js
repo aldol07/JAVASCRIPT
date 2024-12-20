6dc4faef712fe03134c7588a185a5212923bca56
Kartikay Dubey <dubeykartikay13@gmail.com>
2024-12-20 05:51:38 +0000

    numbers done

---
+++ 01_basics/06_num _& _maths.js
@@ -0,0 +1,37 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+//const otherNumber = 23.4564
+//console.log(otherNumber.toPrecision(3));
+
+
+const hundr = 1000000
+
+//console.log(hundr.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-6));
+// console.log(Math.round(45.21));
+// console.log(Math.ceil(45.21));
+// console.log(Math.floor(45.21));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max -  min  + 1 ))+min);
+
+
+
+
