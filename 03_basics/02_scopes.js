ccd7fb230135822ca6485eb2e8986158b6a915db
Kartikay Dubey <dubeykartikay13@gmail.com>
2024-12-23 09:10:53 +0000

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,16 +7,60 @@
 // console.log(c);
 //var c = 300
 
-let a = 300
-if(true) {
-    let a = 10
-    const b = 20
-    var c = 30
-    console.log("Inner ",a);
+// let a = 300
+// if(true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+//     console.log("Inner ",a);
+    
+// }
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one() {
+    const un = "kartik"
+
+    function two() {
+        const web = "wesite"
+        console.log(un);
+        
+    }
+    //console.log(web);
+
+    two()
     
 }
 
+//one()
 
-console.log(a);
-//console.log(b);
-//console.log(c);
+if(true) {
+    const un = "kartik"
+    if(un === "kartik") {
+        const website = "youtube"
+        //console.log(un+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(un);
+/////////+++++++++++++++++++++++INTERESTING++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+
+}
+a//ddone(5)
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+//addTwo(5)
+
