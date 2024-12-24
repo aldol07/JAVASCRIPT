0d2c99ca98dcdb95e4595c96785ed4a4298c7ce8
Kartikay Dubey <dubeykartikay13@gmail.com>
2024-12-24 09:47:15 +0000

    forin done

---
+++ 05_iterations/04.js
@@ -0,0 +1,27 @@
+const myObject  = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift'
+}
+
+for (const key in myObject) {
+
+   // console.log(myObject[key]);
+   //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js","cpp","python"] 
+for (const key in programming) {
+   // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('Ind', "India")
+// map.set('USA', "United states of america")
+// map.set('Fr', "France")
+// for (const key in map) {
+//     console.log(key);
+    
+// }
