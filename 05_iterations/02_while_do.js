c1f2e8989cad5a0c03c8cb130d2bf9f7bedbc376
Kartikay Dubey <dubeykartikay13@gmail.com>
2024-12-24 09:18:54 +0000

    loops done

---
+++ 05_iterations/02_while_do.js
@@ -0,0 +1,25 @@
+// while do while
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2
+    
+    
+}
+
+let myarray = ['flash', 'batman', 'supman'] 
+let arr = 0
+while(arr < myarray.length){
+    //console.log(`Value is ${myarray[arr]}`);
+    arr++
+    
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
